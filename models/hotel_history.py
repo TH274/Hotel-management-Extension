709df4880691827d95418bd5c4b9ba32611243f7
Kitzzu <nekoneko274nb@gmail.com>
2025-01-05 23:54:54 +0700

    Fix model for extension

--- models/hotel_history.py
+++ models/hotel_history.py
@@ -5,12 +5,26 @@ class HotelHotel(models.Model):
 
     booking_ids = fields.One2many('hotel.booking', 'hotel_id', string='Booking History')
 
-
 class HotelBooking(models.Model):
     _name = 'hotel.booking'
     _description = 'Hotel Booking'
+
     guest_name = fields.Char(string='Guest Name', required=True)
     hotel_id = fields.Many2one('hotel.hotel', string='Hotel', required=True)
     room_number = fields.Char(string='Room Number', required=True)
     check_in_date = fields.Date(string='Check-in Date', required=True)
     check_out_date = fields.Date(string='Check-out Date', required=True)
+
+    @api.model
+    def _create_booking_from_customer(self, customer):
+
+        if not customer:
+            return
+
+        self.create({
+            'guest_name': customer.name,
+            'hotel_id': customer.hotel_id.id,
+            'room_number': customer.room_id.room_number,
+            'check_in_date': customer.check_in_date,
+            'check_out_date': customer.check_out_date,
+        })
